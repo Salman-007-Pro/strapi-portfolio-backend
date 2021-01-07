523f316137f0a0cb7ad16f26f06d24f0f99c4427
SalmanDevil <salmanasif36@gmail.com>
2021-01-07 21:03:55 +0500

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6ca35d29484888a9873e3e663adb1bc8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a750da0d-aef5-424a-bba1-18d91968e47d'
+};
